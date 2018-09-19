3a492b4764a8e76e690378e9e3f29e1bcc35f6d2
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-20 00:30:28 +0200

    feat(project): setup tests and add config with Map support

--- src/Library/Config/Config.ts
+++ src/Library/Config/Config.ts
@@ -1,12 +1,63 @@
-import { Homefront } from 'homefront';
-import { ConfigInterface } from './ConfigInterface';
+interface ConfigData { [key: string]: any; }
 
-export class Config extends Homefront {
-  all(): ConfigInterface {
-    return this.getData() as ConfigInterface;
+export class Config {
+  private data: ConfigData;
+
+  constructor(data: ConfigData = {}) {
+    this.data = data;
   }
 
-  of<T> (section: string):T {
-    return this.fetch(section);
+  of<T> (section: string): T {
+    return this.data[section];
+  }
+
+  all (): ConfigData {
+    return this.data;
+  }
+
+  merge (...toMerge: Array<Map<any, any> | ConfigData>) {
+    Config.merge(this.data, ...toMerge);
+  }
+
+  static merge (...toMerge: Array<Map<any, any> | ConfigData>) {
+    const target = toMerge.shift();
+
+    toMerge.forEach(other => Config.patch(target, other));
+  }
+
+  static mergeObject (target: ConfigData, other: ConfigData) {
+    Object.keys(other).reduce((baseData: ConfigData, targetKey: string) => {
+      baseData[targetKey] = Config.patch(baseData[targetKey], other[targetKey]);
+
+      return baseData;
+    }, target);
+
+    return target;
+  }
+
+  static mergeMap (target: Map<any, any>, other: Map<any, any>) {
+    other.forEach((value, key) => {
+      target.set(key, Config.patch(target.get(key), value));
+    });
+
+    return target;
+  }
+
+  static patch (base: any, value: any) {
+    if (typeof value === 'object' && typeof base === 'object') {
+      if (Array.isArray(value) && Array.isArray(base)) {
+        return base.concat(value);
+      }
+
+      if (base.constructor === Object && value.constructor === Object) {
+        return Config.mergeObject(base, value);
+      }
+    }
+
+    if (value instanceof Map && base instanceof Map) {
+      return Config.mergeMap(base, value);
+    }
+
+    return value;
   }
 }
