6996b9c4661e8e33ed204fbd44d762ed4f33cdb1
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-22 20:28:57 +0200

    feat(Server): add robust middleware support

--- src/Library/Server/ServerService.ts
+++ src/Library/Server/ServerService.ts
@@ -8,23 +8,16 @@ import { dispatchMiddleware, requestMiddleware, routerMiddleware } from '../../m
 export const DEFAULT_PORT: number = 1991;
 
 export class ServerService {
-  private server: Koa;
-
-  private readonly config: ServerConfigInterface;
+  private readonly server: Koa;
 
   private readonly application: Application;
 
-  constructor (application: Application, config: ServerConfigInterface = {}) {
+  private config: ServerConfigInterface;
+
+  constructor (application: Application) {
     this.application = application;
-    this.config = config;
 
-    this.create();
-  }
-
-  private create(): this {
     this.server = new Koa();
-
-    return this;
   }
 
   public use(...middlewares: Array<Middleware>): this {
@@ -33,10 +26,40 @@ export class ServerService {
     return this;
   }
 
-  public async initialize(): Promise<void> {
-    const config = this.config;
+  public useBefore(middleware: string | Middleware, ...middlewares: Array<Middleware>): this {
+    return this.updateMiddleware(this.indexOfMiddleware(middleware), 0, ...middlewares);
+  }
 
-    await config.bootstrap(this.server, this);
+  public useAfter(middleware: string | Middleware, ...middlewares: Array<Middleware>): this {
+    return this.updateMiddleware(this.indexOfMiddleware(middleware) + 1, 0, ...middlewares);
+  }
+
+  public replace(middleware: string | Middleware, ...middlewares: Array<Middleware>): this {
+    return this.updateMiddleware(this.indexOfMiddleware(middleware), 1, ...middlewares);
+  }
+
+  public updateMiddleware(at: number, remove: number, ...middlewares: Array<Middleware>): this {
+    const middleware  = this.server.middleware;
+
+    if (at === -1) {
+      throw new Error();
+    }
+
+    middleware.splice(at, remove, ...middlewares);
+
+    return this;
+  }
+
+  public indexOfMiddleware(middleware: string | Middleware) {
+    if (typeof middleware === 'function') {
+      return this.server.middleware.indexOf(middleware);
+    }
+
+    return this.server.middleware.findIndex(suspect => suspect.name === middleware);
+  }
+
+  public async initialize(config: ServerConfigInterface): Promise<void> {
+    this.config = config;
 
     if (config.cors.enabled) {
       this.use(cors(config.cors.options));
