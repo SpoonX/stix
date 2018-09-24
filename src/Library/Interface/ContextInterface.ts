c2ccbd034470f133cc11f4f6df1dcece48b001ca
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-24 18:51:00 +0200

    chore(project): extend context interface

--- src/Library/Interface/ContextInterface.ts
+++ src/Library/Interface/ContextInterface.ts
@@ -1,6 +1,17 @@
 import Koa from 'koa';
+import { Response } from '../Response';
+import { AbstractActionController } from '../Controller';
 
 export interface ContextInterface extends Koa.Context {
-  params?: { [key: string]: any };
+  state: {
+    params?: { [key: string]: any };
+    response?: Response,
+    dispatch?: {
+      controllerName: string;
+      controller: typeof AbstractActionController;
+      action: string;
+    };
+    [key: string]: any;
+  };
   [key: string]: any;
 }
