402b08311a4debc4d9701b6cf10c6c89278bf9da
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-30 18:56:45 +0200

    chore(project): export new paths

--- src/Library/Command/index.ts
+++ src/Library/Command/index.ts
@@ -1,3 +1,6 @@
-export * from './CommandManagerConfigType';
+export * from './AbstractCommand';
 export * from './CommandManager';
+export * from './CommandManagerConfigType';
 export * from './CommandManagerFactory';
+export * from './CommandTypes';
+export * from './HelpCommand';
