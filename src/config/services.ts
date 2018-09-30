6eb8b48d9f4cde668435675f14a6a4c7b69af6ef
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-30 18:57:13 +0200

    chore(project): add new default services

--- src/config/services.ts
+++ src/config/services.ts
@@ -2,17 +2,23 @@ import { ServerService, ServerServiceFactory } from '../Library/Server';
 import { RouterService, RouterServiceFactory } from '../Library/Router';
 import { FactoryInterface } from '../Library/ServiceManager';
 import { ControllerManager, ControllerManagerFactory } from '../Library/Controller';
+import { CommandManager, CommandManagerFactory } from '../Library/Command';
 import { ResponseService, ResponseServiceFactory } from '../Library/Response';
 import { LoggerService, LoggerServiceFactory } from '../Library/Logger';
-import { CliService } from '../Library/Cli';
+import { CliService, CliServiceFactory } from '../Library/Cli';
+import { DispatchMiddleware, RequestMiddleware, RouterMiddleware } from '../Library/Middleware';
 
 export const services = {
   invokables: new Map<Function, Function>([
-    [ CliService, CliService ],
+    [ RouterMiddleware, RouterMiddleware ],
+    [ RequestMiddleware, RequestMiddleware ],
+    [ DispatchMiddleware, DispatchMiddleware ],
   ]),
   factories: new Map<Function,FactoryInterface>([
+    [ CliService, CliServiceFactory ],
     [ ServerService, ServerServiceFactory ],
     [ RouterService, RouterServiceFactory ],
+    [ CommandManager, CommandManagerFactory ],
     [ ControllerManager, ControllerManagerFactory ],
     [ ResponseService, ResponseServiceFactory ],
     [ LoggerService, LoggerServiceFactory ],
