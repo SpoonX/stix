f4e430c6e2761404a13d7cfefcba33dd68039313
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-30 19:06:22 +0200

    chore(project): remove unused file

--- src/Library/Command/index.ts
+++ src/Library/Command/index.ts
@@ -2,5 +2,4 @@ export * from './AbstractCommand';
 export * from './CommandManager';
 export * from './CommandManagerConfigType';
 export * from './CommandManagerFactory';
-export * from './CommandTypes';
 export * from './HelpCommand';
