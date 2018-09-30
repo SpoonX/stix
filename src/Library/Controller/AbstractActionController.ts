8ef1c2b37e54167cf728b47c7d32b4379bc81aac
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-30 19:05:03 +0200

    refactor(Response): remove unneeded class

--- src/Library/Controller/AbstractActionController.ts
+++ src/Library/Controller/AbstractActionController.ts
@@ -1,4 +1,53 @@
-import { AbstractResponseHelper } from '../Response/AbstractResponseHelper';
+import { inject } from '../ServiceManager/decorators';
+import {
+  ClientErrorResponse,
+  RedirectionResponse,
+  ResponseService,
+  ServerErrorResponse,
+  SuccessfulResponse,
+} from '../Response';
 
-export class AbstractActionController extends AbstractResponseHelper {
+export class AbstractActionController {
+  @inject(ResponseService)
+  protected responseService: ResponseService;
+
+  protected getResponseService (): ResponseService {
+    return this.responseService;
+  }
+
+  protected okResponse (data?: any, meta?: any): SuccessfulResponse {
+    return this.responseService.successful().ok(data, meta);
+  }
+
+  protected createdResponse (data?: any, meta?: any): SuccessfulResponse {
+    return this.responseService.successful().created(data, meta);
+  }
+
+  protected notFoundResponse (message?: string, data?: any, meta?: any): ClientErrorResponse {
+    return this.responseService.clientError().notFound(message, data, meta);
+  }
+
+  protected requestTimeoutResponse (message?: string, data?: any, meta?: any): ClientErrorResponse {
+    return this.responseService.clientError().requestTimeout(message, data, meta);
+  }
+
+  protected forbiddenResponse (message?: string, data?: any, meta?: any): ClientErrorResponse {
+    return this.responseService.clientError().forbidden(message, data, meta);
+  }
+
+  protected badRequestResponse (message?: string, data?: any, meta?: Object): ClientErrorResponse {
+    return this.responseService.clientError().badRequest(message, data, meta);
+  }
+
+  protected unauthorizedResponse (message?: string, data?: any, meta?: Object): ClientErrorResponse {
+    return this.responseService.clientError().unauthorized(message, data, meta);
+  }
+
+  protected internalServerErrorResponse (message?: string, data?: any, meta?: Object): ServerErrorResponse {
+    return this.responseService.serverError().internalServerError(message, data, meta);
+  }
+
+  protected permanentRedirectResponse (location: string, alt?: string, meta?: any): RedirectionResponse {
+    return this.responseService.redirection().permanentRedirect(location, alt, meta);
+  }
 }
