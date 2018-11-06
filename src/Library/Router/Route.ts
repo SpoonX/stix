4efa06d5f05f637a59e1365117827087a30d6fe7
RWOverdijk <r.w.overdijk@gmail.com>
2018-11-06 10:55:28 +0100

    fix(Route): fix types for route

--- src/Library/Router/Route.ts
+++ src/Library/Router/Route.ts
@@ -1,27 +1,29 @@
 import { RequestMethods } from '../Server';
+import { RouteInterface } from './RouteInterface';
+import { AbstractActionController } from '../Controller';
 
 export class Route {
-  public static method (method: string, route: string, controller: string | { new() : Object }, action: string): {} {
-    return { method, route, controller, action };
+  public static method (method: RequestMethods, route: string, controller: string | typeof AbstractActionController, action: string): RouteInterface {
+    return { method, route, action, controller: controller as typeof AbstractActionController };
   }
 
-  public static get (route: string, controller: string | { new() : Object }, action: string): {} {
+  public static get (route: string, controller: string | typeof AbstractActionController, action: string): RouteInterface {
     return Route.method(RequestMethods.Get, route, controller, action);
   }
 
-  public static post (route: string, controller: string | { new() : Object }, action: string): {} {
+  public static post (route: string, controller: string | typeof AbstractActionController, action: string): RouteInterface {
     return Route.method(RequestMethods.Post, route, controller, action);
   }
 
-  public static put (route: string, controller: string | { new() : Object }, action: string): {} {
+  public static put (route: string, controller: string | typeof AbstractActionController, action: string): RouteInterface {
     return Route.method(RequestMethods.Put, route, controller, action);
   }
 
-  public static patch (route: string, controller: string | { new() : Object }, action: string): {} {
+  public static patch (route: string, controller: string | typeof AbstractActionController, action: string): RouteInterface {
     return Route.method(RequestMethods.Patch, route, controller, action);
   }
 
-  public static delete (route: string, controller: string | { new() : Object }, action: string): {} {
+  public static delete (route: string, controller: string | typeof AbstractActionController, action: string): RouteInterface {
     return Route.method(RequestMethods.Delete, route, controller, action);
   }
 }
